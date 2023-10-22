443d71001d2c933332d582d954aec6a24cb93dde
Kartik Neti <kartikprajak@gmail.com>
2023-10-22 21:16:21 +0530

    Completed pdf merge

--- pdf merge/main.py
+++ pdf merge/main.py
@@ -8,18 +8,17 @@ print(filename)
 
 for f in filename:
     name = f.capitalize()
-    # name = name.split('.')[0]
-    # print(name)
+    
     pdf.add_page()
     pdf.set_font(family='Times', size=20, style='BIU')  
-    pdf.cell(align='L', w=0, h=12, txt=name.split('.')[0])
+    pdf.cell(w=0, h=12, txt=name.split('.')[0])
     # print(f)
     pdf.ln(30)
 
     with open(file=f) as file:
-        text = file.readline()
+        text = file.read()
     pdf.set_font(family='Times', size=12)     
-    pdf.multi_cell(align='L', w=200, h=12, border= 1, txt=text)
+    pdf.multi_cell(w=0, h=12, txt=text)
         
         
     
